d3a638972ad6e141f518ed603c970bc2d6a75cac
Priyanshu <priyanshu1.sharma@paytm.com>
2024-01-29 18:57:42 +0530

    final changes

--- Backend/db.js
+++ Backend/db.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose");
 
-url="mongodb+srv://priyanshu8sh:8soNVix1iswbrjJn@cluster0.flo0pig.mongodb.net/todos";
+url="mongodb+srv://*********@cluster0.flo0pig.mongodb.net/todos";
 mongoose.connect(url);
 
 const todoSchema=mongoose.Schema({
--- Frontend/src/components/CreateTodo.jsx
+++ Frontend/src/components/CreateTodo.jsx
@@ -1,12 +1,22 @@
-
+import { useState } from "react";
+import axios from "axios";
 export function CreateTodo()
 {
-    
+    const [title,setTitle]=useState("");
+    const [description,setDescription]=useState("");
+
+    const addTodo=(e)=>{
+        console.log("in add todo");
+        const newData={"title":title,"description":description};
+        axios.post("http://localhost:3001/todo", newData);
+            
+
+    }
     return (
         <div>
-            <input type="text" placeholder="title"></input><br></br>
-            <input type="text" placeholder="description"></input><br></br>
-            <button >Add a todo</button> 
+            <input type="text" placeholder="title" onChange={(e) => setTitle(e.target.value)}></input><br></br>
+            <input type="text" placeholder="description" onChange={(e) => setDescription(e.target.value)}></input><br></br>
+            <button onClick={addTodo} >Add a todo</button> 
         </div>
-    )
+    );
 }
